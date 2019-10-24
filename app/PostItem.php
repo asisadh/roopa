1aff5d746519929594d30477773e2adee3c9de5a
Shiraj Pradhanang <ceeraazz@gmail.com>
2019-10-24 17:03:11 +0545

    postitem model updated

--- app/PostItem.php
+++ app/PostItem.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostItem extends Model
 {
-
+    protected $fillable = [
+        'user_id',
+        'title',
+        'images',
+        'price',
+        'pricing',
+        'type',
+        'street_address',
+        'area_address',
+        'city_address',
+        'number_of_bed_rooms',
+        'number_of_wash_room',
+        'total_area',
+        'floor',
+        'amenities',
+        'description',
+        'longitude',
+        'latitude',
+        'is_sold'
+    ];
 }
